a361a88e4cbd038fb8f9cba41c839e0adc2cad8e
cQu1x <b.nilov@innopolis.university>
2025-06-21 23:46:49 +0300

    Delete frontend directory

--- frontend/lib/presentation/themes/dark_theme.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DarkThemeColors {
-  static const backgroundColor = Color(0xFF1E1E1E);
-  static const borderColor = Colors.transparent;
-  static const primaryColor = Colors.black;
-}
